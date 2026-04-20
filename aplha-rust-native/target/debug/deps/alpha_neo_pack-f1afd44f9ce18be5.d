4a5e213649db5382af2525369451293d6909de54
Jakub Kolcar <j.kolcar@raynet.cz>
2026-04-20 10:07:18 +0200

    Ignore native Rust build artifacts

--- aplha-rust-native/target/debug/deps/alpha_neo_pack-f1afd44f9ce18be5.d
+++
@@ -1,5 +0,0 @@
-/Users/jakubkolcar/customs/neo-re/aplha-rust-native/target/debug/deps/alpha_neo_pack-f1afd44f9ce18be5.d: crates/alpha-neo-pack/src/main.rs
-
-/Users/jakubkolcar/customs/neo-re/aplha-rust-native/target/debug/deps/libalpha_neo_pack-f1afd44f9ce18be5.rmeta: crates/alpha-neo-pack/src/main.rs
-
-crates/alpha-neo-pack/src/main.rs:
--- aplha-rust-native/target/debug/deps/alpha_usb_applet-3f33cbae8cf919dc.d
+++
@@ -1,5 +0,0 @@
-/Users/jakubkolcar/customs/neo-re/aplha-rust-native/target/debug/deps/alpha_usb_applet-3f33cbae8cf919dc.d: applets/alpha_usb/src/lib.rs
-
-/Users/jakubkolcar/customs/neo-re/aplha-rust-native/target/debug/deps/libalpha_usb_applet-3f33cbae8cf919dc.rmeta: applets/alpha_usb/src/lib.rs
-
-applets/alpha_usb/src/lib.rs:
